07ab69cf2b39d0f2a880a0cacaf3bf6846176e49
Nirav <niravnvk@gmail.com>
2015-02-03 16:48:18 +0530

    s

---
+++ app/src/main/java/com/webmyne/paylabasmerchant/ui/CommissionClaimActivity.java
@@ -0,0 +1,18 @@
+package com.webmyne.paylabasmerchant.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.webmyne.paylabasmerchant.R;
+
+public class CommissionClaimActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commission_claim);
+    }
+
+
+
+}
